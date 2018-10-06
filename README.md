47f2d22388a4352fc34f3479e4c191b59f06b0f3
Dariusz Dwornikowski <dariusz.dwornikowski@cs.put.poznan.pl>
2018-10-06 11:47:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Azure GO SDK Example 
+
+This program gets a list of all resource groups in a subscription and list VMs in every resource group. 
+
+## Prerequisities 
+
+Program assumes you do have an Azure account, Azure CLI and a working go installation with dep installed. 
+
+## Preparing
+
+Before using or changing the code, run:
+
+```dep ensure``` 
+
+Create service principal file:
+
+```az ad sp create-for-rbac -sdk-auth > my.auth```
+
+And export path to the authorizer:
+
+```export AZURE_AUTH_LOCATION=my.auth```
+
+## Running
+
+Just run with `go run main.go` 
+
+## Licence 
+
+Dariusz Dwornikowski, Nordcloud, MIT
---
+++ main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"sync"
+
+	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2018-06-01/compute"
+	"github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2018-02-01/resources"
+
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest/azure"
+	"github.com/Azure/go-autorest/autorest/azure/auth"
+	"github.com/pkg/errors"
+)
+
+// AzureSession is an object representing session for subscription
+type AzureSession struct {
+	SubscriptionID string
+	Authorizer     autorest.Authorizer
+}
+
+func readJSON(path string) (*map[string]interface{}, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, errors.Wrap(err, "Can't open the file")
+	}
+	contents := make(map[string]interface{})
+	err = json.Unmarshal(data, &contents)
+
+	if err != nil {
+		err = errors.Wrap(err, "Can't not unmarshal file")
+	}
+
+	return &contents, err
+}
+
+func newSessionFromFile() (*AzureSession, error) {
+	authorizer, err := auth.NewAuthorizerFromFile(azure.PublicCloud.ResourceManagerEndpoint)
+
+	if err != nil {
+		return nil, errors.Wrap(err, "Can't initialize authorizer ☹️")
+	}
+	authInfo, err := readJSON(os.Getenv("AZURE_AUTH_LOCATION"))
+
+	if err != nil {
+		return nil, errors.Wrap(err, "Can''t get authinfo")
+	}
+	sess := AzureSession{
+		SubscriptionID: (*authInfo)["subscriptionId"].(string),
+		Authorizer:     authorizer,
+	}
+
+	return &sess, err
+}
+
+func getGroups(sess *AzureSession) ([]string, error) {
+	tab := make([]string, 0)
+	var err error
+
+	grClient := resources.NewGroupsClient(sess.SubscriptionID)
+	grClient.Authorizer = sess.Authorizer
+
+	for list, err := grClient.ListComplete(context.Background(), "", nil); list.NotDone(); err = list.Next() {
+		if err != nil {
+			return nil, errors.Wrap(err, "got error while traverising RG list")
+		}
+		rgName := *list.Value().Name
+		tab = append(tab, rgName)
+	}
+	return tab, err
+}
+
+func getVM(sess *AzureSession, rg string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	vmClient := compute.NewVirtualMachinesClient(sess.SubscriptionID)
+	vmClient.Authorizer = sess.Authorizer
+
+	for vm, err := vmClient.ListComplete(context.Background(), rg); vm.NotDone(); err = vm.Next() {
+		if err != nil {
+			log.Print(err, "got error while traverising RG list")
+		}
+		i := vm.Value()
+		fmt.Printf("(%s) VM %s\n", rg, *i.Name)
+	}
+}
+
+func main() {
+
+	var wg sync.WaitGroup
+	sess, err := newSessionFromFile()
+
+	if err != nil {
+		log.Fatal("Can't create session: ", err)
+	}
+	groups, err := getGroups(sess)
+
+	if err != nil {
+		log.Fatal("Can't get resource groups")
+	}
+
+	wg.Add(len(groups))
+
+	for _, group := range groups {
+		go getVM(sess, group, &wg)
+	}
+	wg.Wait()
+
+}
